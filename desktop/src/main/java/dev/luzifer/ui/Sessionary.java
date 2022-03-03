a235900e7d6852a6aeb729ce0b2e82f9281a1c86
Luziferium <luziferisdoof@gmail.com>
2022-03-03 15:10:38 +0100

    Added logo, ViewController and created the Application representer class

---
+++ desktop/src/main/java/dev/luzifer/ui/Sessionary.java
@@ -0,0 +1,15 @@
+package dev.luzifer.ui;
+
+import dev.luzifer.ui.view.ViewController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Sessionary extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        ViewController viewController = new ViewController(primaryStage);
+        // TODO:
+    }
+}
---
+++ desktop/src/main/java/dev/luzifer/ui/view/ViewController.java
@@ -0,0 +1,50 @@
+package dev.luzifer.ui.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.MessageFormat;
+
+public class ViewController {
+
+    private final Stage stage;
+
+    public ViewController(Stage stage) {
+        this.stage = stage;
+    }
+
+    private <T> void loadAndShowView(Class<T> clazz, Callback<Class<?>, Object> controllerFactory, String title) {
+
+        Parent root = loadView(clazz, controllerFactory);
+        Scene scene = new Scene(root);
+
+        stage.getIcons().addAll(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private <T> Parent loadView(Class<T> clazz, Callback<Class<?>, Object> controllerFactory) {
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+
+        URL fxmlLocation = clazz.getResource(clazz.getSimpleName() + ".fxml");
+
+        fxmlLoader.setLocation(fxmlLocation);
+        fxmlLoader.setControllerFactory(controllerFactory);
+
+        try {
+            return fxmlLoader.load();
+        } catch (IOException e) {
+            throw new IllegalStateException(MessageFormat.format("FXML could not get loaded for class: {}", clazz), e);
+        }
+    }
+
+}
