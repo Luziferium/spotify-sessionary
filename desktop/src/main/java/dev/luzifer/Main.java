288b02db282ac4154904119792bc775faa2dce20
Luziferium <luziferisdoof@gmail.com>
2022-03-03 15:10:58 +0100

    Forgot the main, bruh

---
+++ desktop/src/main/java/dev/luzifer/Main.java
@@ -0,0 +1,12 @@
+package dev.luzifer;
+
+import dev.luzifer.ui.Sessionary;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(Sessionary.class);
+    }
+
+}
