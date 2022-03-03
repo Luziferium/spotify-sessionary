a1a66710d027c131989a5bb848cd01a97c8cda44
Luziferium <luziferisdoof@gmail.com>
2022-03-03 16:12:43 +0100

    Did some testings with backgrounds

--- desktop/src/main/java/dev/luzifer/ui/view/views/LoginView.java
+++ desktop/src/main/java/dev/luzifer/ui/view/views/LoginView.java
@@ -1,4 +1,34 @@
 package dev.luzifer.ui.view.views;
 
-public class LoginView {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginView implements Initializable {
+
+    @FXML
+    private Label backgroundLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        backgroundLabel.setBackground(new Background(backgroundImage()));
+    }
+
+    private BackgroundImage backgroundImage() {
+
+        Image image = new Image(getClass().getClassLoader().getResource("logo.png").toExternalForm());
+
+        return new BackgroundImage(
+                image,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
+                new BackgroundPosition(Side.LEFT, 0, true, Side.BOTTOM, 0, true),
+                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, false, true)
+        );
+    }
 }
