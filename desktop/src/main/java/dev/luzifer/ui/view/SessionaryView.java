056f1f5235e0d752aced30902c14deccace26526
Luziferium <luziferisdoof@gmail.com>
2022-03-05 11:38:15 +0100

    Changed SessionaryView

--- desktop/src/main/java/dev/luzifer/ui/view/SessionaryView.java
+++ desktop/src/main/java/dev/luzifer/ui/view/SessionaryView.java
@@ -2,10 +2,15 @@ package dev.luzifer.ui.view;
 
 import dev.luzifer.ui.util.ImageHelper;
 import dev.luzifer.ui.viewmodel.ViewModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Separator;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,19 +21,25 @@ public class SessionaryView extends View {
     private Pane pane;
     
     @FXML
-    private Separator seperator1;
+    private ProgressBar songProgress;
     
     @FXML
-    private Separator seperator2;
+    private Button playPauseButton;
     
     @FXML
-    private AnchorPane anchorPane1;
+    private Circle logoShape;
     
     @FXML
-    private AnchorPane anchorPane2;
+    private Label nameLabel;
     
     @FXML
-    private AnchorPane anchorPane3;
+    private ListView<?> playlistList;
+    
+    @FXML
+    private Separator verticalSeparator;
+    
+    @FXML
+    private Separator horizontalSeparator;
     
     public SessionaryView(ViewModel viewModel) {
         super(viewModel);
@@ -36,6 +47,13 @@ public class SessionaryView extends View {
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+        ImageHelper.setFillImage(logoShape, "logo.png");
         ImageHelper.setBackgroundImage(pane, "background.png");
+    }
+    
+    @FXML
+    void onButtonPress(ActionEvent event) {
+    
     }
 }
