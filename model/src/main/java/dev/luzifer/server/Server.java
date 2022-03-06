399972e7fb3db77587739aa0d18b9d224a4704d3
Luziferium <luziferisdoof@gmail.com>
2022-03-06 18:50:52 +0100

    Gave Server an IP

--- model/src/main/java/dev/luzifer/server/Server.java
+++ model/src/main/java/dev/luzifer/server/Server.java
@@ -2,4 +2,13 @@ package dev.luzifer.server;
 
 public class Server {
 
+    private final String ip;
+    
+    public Server(String ip) {
+        this.ip = ip;
+    }
+    
+    public String getIp() {
+        return ip;
+    }
 }
