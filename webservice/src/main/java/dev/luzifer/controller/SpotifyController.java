b2234d237ec67e7538d8b70612028b4c926c8074
Luziferium <luziferisdoof@gmail.com>
2022-03-07 18:08:23 +0100

    Implemented Controller templates, removed WebService.java

--- webservice/src/main/java/dev/luzifer/controller/SpotifyController.java
+++ webservice/src/main/java/dev/luzifer/controller/SpotifyController.java
@@ -1,8 +1,7 @@
-package dev.luzifer.service;
+package dev.luzifer.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WebService {
-
+public class SpotifyController {
 }
