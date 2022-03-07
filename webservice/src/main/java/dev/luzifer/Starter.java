09c7bbfeed7f176714790148e31e3a05d3f9f9e6
Luziferium <luziferisdoof@gmail.com>
2022-03-07 18:01:01 +0100

    Starter now runs the Webservice

--- webservice/src/main/java/dev/luzifer/Starter.java
+++ webservice/src/main/java/dev/luzifer/Starter.java
@@ -1,9 +1,12 @@
 package dev.luzifer;
 
+import dev.luzifer.service.WebService;
+import org.springframework.boot.SpringApplication;
+
 public class Starter {
     
     public static void main(String[] args) {
-    
+        SpringApplication.run(WebService.class, args);
     }
     
 }
