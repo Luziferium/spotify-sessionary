45fbba709df89b1c3470cb348809cf5013987f60
Luziferium <luziferisdoof@gmail.com>
2022-03-09 08:22:42 +0100

    Made Session class for later purpose

--- desktop/src/main/java/dev/luzifer/ui/SessionController.java
+++ desktop/src/main/java/dev/luzifer/ui/SessionController.java
@@ -1,4 +1,16 @@
 package dev.luzifer.ui;
 
+import dev.luzifer.session.Session;
+
+/**
+ * Management class for {@link dev.luzifer.session.Session}
+ */
 public class SessionController {
+    
+    private final Session session;
+    
+    public SessionController() {
+        this.session = new Session();
+    }
+    
 }
